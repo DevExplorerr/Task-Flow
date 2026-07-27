e48a8dafa4126cf4867d143c1c7e77f11f196e10
DevExplorerr <saadj4775@gmail.com>
2026-07-27 16:50:52 +0500

    Refactor Firebase configuration: update firebase.json and restructure firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:task_management_app/firebase/firebase_options.dart';
+import 'package:task_management_app/firebase_options.dart';
 import 'package:task_management_app/logic/provider/settings_provider.dart';
 import 'package:task_management_app/logic/provider/theme_provider.dart';
 import 'package:task_management_app/logic/services/auth_service.dart';
